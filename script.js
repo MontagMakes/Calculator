2b00acc20cf1af4a4b504b2313d270565fb3e20d
SolitaryXero <103876121+SolitaryXero@users.noreply.github.com>
2023-10-29 10:07:28 +0500

    number and operations completed

--- script.js
+++ script.js
@@ -4,11 +4,12 @@ const btnArray = ["AC", "+/-", "%", "/", 7, 8, 9, "*", 4, 5, 6, "-",
                 1, 2, 3, "+", "del", 0, ".", "=" ]
 const operators = ["/", "*", "-", "+"]
 
-let num1;
-let num2;
-let operator;
-let isNum2 = false;
+let num1 = null;
+let num2 = null;
+let operator = null;
+let isNumFirst = true;
 
+createButtons();
 //create Buttons and their style
 function createButtons(){
     for(let i=0;i<20;i++){
@@ -31,70 +32,104 @@ function displayText(square, i){
     //display numbers
     if(square.textContent >= 0 && square.textContent <= 9){
         square.addEventListener("click", ()=>{
-            if(operator == undefined){
-                if(display.textContent == 0){
+            if(num1 == null && num2 == null && operator == null){
+                console.log(isNumFirst)
+                if(isNumFirst){
                     display.textContent = btnArray[i];
                     num1 = display.textContent;
+                    isNumFirst = false;
                 } else {
                     display.textContent += btnArray[i];
                     num1 = display.textContent;
                 }
-            } else {
-                display.textContent = 0;
-                if(display.textContent == 0){
+
+            } else if(num1 != null && operator != null && num2 == null) {
+                if(isNumFirst){
                     display.textContent = btnArray[i];
                     num2 = display.textContent;
+                    isNumFirst = false;
                 } else {
                     display.textContent += btnArray[i];
                     num2 = display.textContent;
                 }
             }
-            console.log(`num1: ${num1}, num2: ${num2}, operator: ${operator}`)
         })
     }
 
     if(square.textContent == "+" || square.textContent == "-" ||
        square.textContent == "*" || square.textContent == "/"){
         square.addEventListener("click", ()=>{
-            operator = square.textContent;
-            console.log(operator)
+            if(num1 != null && num2 == null && operator == null){
+                
+                display.textContent = 0;
+                operator = square.textContent;
+                isNumFirst = true;
+
+            } else if (num1 != null && num2 != null && operator != null){
+                display.textContent = operate(num1, operator, num2);
+                
+                num1 = display.textContent;
+                num2 = null;
+                operator = square.textContent;
+                isNumFirst = true;
+                
+            } else if (num1 != null && num2 == null && operator != null){
+                operator = square.textContent;
+            }
+
         })
     }
-    
 
-    
+    if(square.textContent == "="){
+        square.addEventListener("click", ()=>{
+            display.textContent = operate(num1, operator, num2);
+            console.log(`num1: ${num1}, num2: ${num2}, operator: ${operator}`)
+            
+            num1 = display.textContent;
+            num2 = null;
+            operator = null;
+            
+        })
+    }
+
+    if(square.textContent == "AC"){
+        square.addEventListener("click", ()=>{
+            display.textContent = 0;
+            num1 = null;
+            num2 = null;
+            operator = null;
+            isNumFirst = true;
+        })
+    }
 }
-createButtons();
 
 function operate(num1, operator, num2){
     if(operator == "+"){
-        operator = null;
         return add(num1, num2);
     } else if (operator == "-"){
-        operator = null;
+        
         return subtract(num1, num2);
     } else if (operator == "*"){
-        operator = null;
+        
         return multiply(num1, num2);
     } else {
-        operator = null;
+        
         return divide(num1, num2);
     }
 }
 
-
 function add(num1, num2){
-    return num1 + num2;
+    return Number(num1) + Number(num2);
 }
 
 function multiply(num1, num2){
-    return num1 * num2;
+    return Number(num1) * Number(num2);
 }
 
 function subtract(num1, num2){
-    return num1 - num2
+    return Number(num1) - Number(num2)
 }
 
 function divide(num1, num2){
-    return num1 / num2;
+    return Number(num1) / Number(num2);
 }
