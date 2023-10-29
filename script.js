09a4a4ca894982688d3d5048f38e55095442397a
SolitaryXero <103876121+SolitaryXero@users.noreply.github.com>
2023-10-29 12:56:03 +0500

    negative sign incomplete

--- script.js
+++ script.js
@@ -110,11 +110,21 @@ function displayText(square, i){
         })
     }
 
+    // equals button
     if(square.textContent == "="){
         square.addEventListener("click", ()=>{
-            display.textContent = operate(num1, operator, num2);
+            let result = operate(num1, operator, num2);
+            if(String(result).includes("-")){
+                display.textContent = String(result).substring(1,result.length) + "-"
+
+            }else {
+                display.textContent = result;
+            }
+            console.log(String(result).includes("-"))
+            display.textContent = result;
             
-            num1 = display.textContent;
+            
+            num1 = display.textContent
             num2 = null;
             operator = null;
             isNumFirst = true;
@@ -122,7 +132,7 @@ function displayText(square, i){
         })
     }
 
-    //clear
+    //clear button
     if(square.textContent == "AC"){
         square.addEventListener("click", ()=>{
             display.textContent = 0;
@@ -133,6 +143,7 @@ function displayText(square, i){
         })
     }
     
+    // delete button
     if(square.textContent == "del"){
         square.addEventListener("click", ()=>{
             console.log(display.textContent.length);
@@ -151,11 +162,48 @@ function displayText(square, i){
             }
         })
     }
+
+    // percentage button
+    if(square.textContent == "%"){
+        square.addEventListener("click", ()=>{
+            display.textContent = display.textContent / 100;
+            if(num1 != null){
+                num1 = display.textContent;
+            } else if (num2 != null){
+                num2 = display.textContent;
+            }
+            console.log(`num1: ${num1}, num2: ${num2}, operator: ${operator} isNumFirst: ${isNumFirst}`)
+
+        })
+    }
+    //+/- button
+    if(square.textContent == "+/-"){
+        square.addEventListener("click", ()=>{
+            if(!display.textContent.includes("-")){
+
+                display.textContent = "-" + display.textContent
+            } else if (display.textContent.includes("-")){
+                display.textContent = display.textContent.substring(1, display.textContent.length)
+            }
+            
+        })
+    }
 }
 
 function operate(num1, operator, num2){
     if(operator == "+"){
-        return add(num1, num2);
+        result = add(num1, num2)
+        
+        if(String(result).includes("-")){
+            
+            result =  String(result).substring(0, result.length-1);
+            return result 
+        }else {
+            console.log(result)
+            return result
+        }
+        
+        
     } else if (operator == "-"){
         
         return subtract(num1, num2);
